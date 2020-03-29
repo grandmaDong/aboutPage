b084c97f4699198d061df0284e697b7f2b810de7
dwj <1372508407@qq.com>
2020-03-29 11:23:03 +0800

    fix build file

---
+++ webpackConfig/webpack.base.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|tsx?)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              // publicPath: './',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'less-loader' // 不使用less-loader 也能处理文件中包含的less？？？
+        ],
+      },
+      {
+        test:/\.css$/,
+        exclude:/src/,
+        use:[
+            { loader: "style-loader",},
+            {
+                loader: "css-loader",
+                options:{
+                    importLoaders:1
+                }
+            }
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        use: 'url-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  }
+}
---
+++ webpackConfig/webpack.prod.config.js
@@ -0,0 +1,29 @@
+const webpackBaseConfig = require('./webpack.base.config.js')
+const merge = require('webpack-merge')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = merge(webpackBaseConfig, {
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'prod environment',
+      inject: true,
+      template: '!!ejs-loader!./src/template/index.ejs'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: './assets/index.css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ]
+})
